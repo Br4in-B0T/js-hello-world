5022cf8931694bb6d9d0817b76924775b1b9fb3c
Colin <thecbrainerd@gmail.com>
2024-05-23 09:59:36 -0500

    Bonus Questions Complete

--- hello.js
+++ hello.js
@@ -3,8 +3,10 @@ Welcome, new coder.
 Enter your code on line 7 to print 'Hello, World!' 
 to the console (the panel on the right)
 */
+const mssg = 'I\nam\nIron Man'
 
-console.log("Hello");
+
+console.log("Hello, World!");
 
 /*  
 Next, PLAY! 
@@ -20,3 +22,36 @@ Modify your code on line 7 to try to accomplish the tasks listed below:
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
    9. Other. You choose!
 */
+
+
+console.log(mssg);
+/*
+if (counter < 5) {
+  console.log(counter++);
+}
+*/
+
+for (let counter = 0 ;counter <= 5; ++counter) {
+  if (counter === 0){
+    console.log("Hello");
+  } else if (counter === 1){
+    console.log("World");
+  } else if (counter === 2){
+    console.log("My");
+  } else if (counter === 3){
+    console.log("Name");
+  } else if (counter === 4){
+    console.log("IS");
+  } else if (counter === 5){
+    console.log('"COLIN"');
+  } else{
+    break;
+  }
+}
+
+const message1 = "It's nice to ";
+const message2 = "meet you!";
+
+const two_in_one = message1 + message2
+
+console.log(two_in_one)
